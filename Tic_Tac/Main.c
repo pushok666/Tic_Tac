853c2459372941905fe5bf1702c7c0b7065ca9bf
pushok666 <pavel.work.01@bk.ru>
2022-01-05 06:37:19 -0500

    init palyground

--- Tic_Tac/Main.c
+++ Tic_Tac/Main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+const int H = 3;
+const int W = 3; // size playground
+char myPlayground[3][3];
+void init_playground(void);
 
 int main(void)
 {
-	printf("Hhhhhelloooo \n");
+	printf("Hellooo \n");
+	init_playground();
 	return 0;
+}
+
+void init_playground(void)
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		/* code */
+		for(int y = 0; y < W; ++y)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 }
